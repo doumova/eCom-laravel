10aaf1889c46587e62857902abff55f874d08fb2
doumova <doumogueye.mg@gmail.com>
2021-02-25 00:57:19 +0100

    commit
    Signed-off-by: doumova <doumogueye.mg@gmail.com>

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    function login (Request $req)
+    {
+        return User::where(['email'=>$req->email])->first();
+    }
+}
