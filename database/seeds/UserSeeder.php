36465899eae09a95562949ad418066acc7efd649
doumova <doumogueye.mg@gmail.com>
2021-02-25 01:40:44 +0100

    controlle login && redirect page

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
         //
         DB::table('users')->insert([
             'name'=>'soda gueye',
-            'email'=>'soda@gmail.com',
+            'email'=>'@gmail.com',
             'password'=>Hash::make('12345')
         ]);
     }
